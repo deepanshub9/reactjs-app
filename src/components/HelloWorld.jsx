d8ebb7e6552c7800595d8cfff11af354a31e9314
Deepanshu <kukukumar1996@gmail.com>
2025-02-25 14:12:19 +0000

    New change1

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HelloWorld = () => {
   return (
     <div className="hello-container">
-      <h1 className="hello-text">CI/CD Application Deploy with Jenkins Server</h1>
+      <h1 className="hello-text">React.js Application Deploy with Jenkins Server</h1>
       
     </div>
   );
